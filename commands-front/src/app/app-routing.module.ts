d22393e9e172d8c0b5ee97f9796efa5838b3b799
Arina Kataeva <123296525+arinakataeva007@users.noreply.github.com>
2025-04-11 22:28:17 +0500

    feat: модальные окна и роутинг

--- commands-front/src/app/app-routing.module.ts
+++ commands-front/src/app/app-routing.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { LogInModalComponent } from './modal-windows/log-in-modal/log-in-modal.component';
+import { SignInModalComponent } from './modal-windows/sign-in-modal/sign-in-modal.component';
 
-const routes: Routes = [{
-  path: '',
+const routes: Routes = [
+  {
+    path: '',
     component: AppComponent,
     children: [
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: 'welcome', component: WelcomePageComponent },
-    ]
-}];
+      { path: 'logIn', component: LogInModalComponent },
+      { path: 'signIn', component: SignInModalComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- commands-front/src/app/pages/welcome-page/welcome-page.component.ts
+++ commands-front/src/app/pages/welcome-page/welcome-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent {
-
+  constructor(private router: Router){}
+  protected goToLogIn(){
+    this.router.navigate(['/logIn']);
+  }
+  protected goToSignIn(){
+    this.router.navigate(['/signIn']);
+  }
 }
