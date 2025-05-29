0803d94fe8dbc3ca951aaa8d2876a450510c1597
Arina Kataeva <123296525+arinakataeva007@users.noreply.github.com>
2025-05-30 01:22:42 +0500

    refactor gpt

--- Commands-back/Commands-back/Program.cs
+++ Commands-back/Commands-back/Program.cs
@@ -5,25 +5,24 @@ using Commands_back.Services;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
-
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build(); // app - готовое веб-приложение 
-// Добавляем сервисы API и Swagger
-builder.Services.AddControllers(); // регистрирует сервисы для api-контроллеров
+
+builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 builder.Services.AddScoped<IRoleService, RoleService>();
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
-builder.Services.AddEndpointsApiExplorer(); // сканирует контроллеры и создает описание api в свагере
-builder.Services.AddSwaggerGen(); // добавляет свагер
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<Commands_back.Services.PasswordHasher>();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -33,26 +32,19 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod();
     });
 });
+
+var app = builder.Build();
+app.UseHttpsRedirection();
 app.UseCors(); 
-app.UseRouting(); // вкл систему маршрутизации
-app.UseHttpsRedirection(); // включение https и маршрутизации
+app.UseRouting();
+app.UseAuthorization(); 
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
         Path.Combine(builder.Environment.WebRootPath, "uploads")),
     RequestPath = "/uploads"
 });
-
-
-// ReSharper disable once InvalidXmlDocComment
-
-/**
- * Включение свагера
- */
 app.UseSwagger();
 app.UseSwaggerUI();
-
-// Говорит приложению обрабатывать запросы к API-контроллерам
 app.MapControllers();
-
 app.Run();
